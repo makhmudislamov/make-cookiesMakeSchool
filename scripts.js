239cb87767856f162525272f7dc6dd21f35c9cb2
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-10-06 14:19:30 -0700

    cookie facilites

--- scripts.js
+++ scripts.js
@@ -142,3 +142,68 @@ let autoGrandmaStart = function () {
         refreshCookieCount();
     }, 1000);
 }
+
+/********************************
+
+          Facilities
+
+********************************/
+
+//set default variables
+let facilityAuto = false;
+let facilityPower = 2000;
+let facilityPriceAmount = 100000;
+let facilityLevelNumber = 0;
+
+//declare DOM variables
+let buyFacility = document.getElementById('buy-facility');
+let facilityPrice = document.getElementById('facility-price');
+let facilityLevel = document.getElementById('facility-level');
+let facilityMultiple = document.getElementById('facility-multiple');
+
+//buy a facility
+buyFacility.addEventListener("click", function () {
+    //set autoLoop to false
+    facilityAuto = false;
+
+    //make sure we have enough cookies
+    if (cookieCount >= facilityPriceAmount) {
+        cookieCount -= facilityPriceAmount;
+        refreshCookieCount()
+
+        //upgrade power level
+        facilityLevelNumber += 1;
+
+        //update price
+        facilityPriceAmount = Math.floor(facilityPriceAmount * 1.33);
+
+        //update facility power
+        facilityPower += 600;
+
+        //turn autoFacility on!
+        facilityAuto = true
+        autoFacilityStart();
+
+        //refresh shop item
+        refreshFacility();
+
+    }//DONT FORGET THIS BRACKET - I didnt
+
+})
+
+//refresh shop
+let refreshFacility = function () {
+    facilityLevel.innerHTML = facilityLevelNumber
+    facilityPrice.innerHTML = facilityPriceAmount;
+    facilityMultiple.innerHTML = facilityPower - 600;
+}
+
+//game loop
+let autoFacilityStart = function () {
+    let facilityInt = window.setInterval(function () {
+        cookieCount += facilityPower;
+        refreshCookieCount();
+    }, 1000);
+}
+
+//refresh shop
